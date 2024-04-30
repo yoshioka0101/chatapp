bb072dca530056a010535f6d78edabbe2a047c8f
Shishidoyuuki <yuuki0206nano@gmail.com>
2024-04-30 18:51:11 +0900

    チャンネル機能

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,18 @@
+class ChatChannel < ApplicationCable::Channel
+# https://xrpl.org/ja/docs/references/http-websocket-apis/public-api-methods/subscription-methods/
+  def subscribed
+    stream_from 'chat_channel'
+  end
+# https://xrpl.org/ja/docs/references/http-websocket-apis/public-api-methods/subscription-methods/unsubscribe/
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    user = User.find_by(email: data['email'])
+    
+    if message = Message.create(content: data['message'], user_id: user.id)
+      ActionCable.server.broadcast 'chat_channel', { message: data['message'], name: user.name, created_at: message.created_at }
+    end
+  end
+end
