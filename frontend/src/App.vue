4d5d29f361df4ad179b70de6c44b400d0cbe0c06
Shishidoyuuki <yuuki0206nano@gmail.com>
2024-05-01 04:54:11 +0900

    ログイン機能の実装_vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,3 @@
 <template>
   <router-view/>
 </template>
-
-<script>
-export default {
-}
-</script>
-
-<style scoped>
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome'
+import Chatroom from '../views/Chatroom'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/chatroom',
+    name: 'Chatroom',
+    component: Chatroom
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
