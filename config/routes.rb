b5a75a3b58c728aa7de374185e1009befb92c7e3
Shishidoyuuki <yuuki0206nano@gmail.com>
2024-04-30 17:32:41 +0900

    うまくいった！！
    - メッセージ機能がうまくいった
    - Controllerやってないだけだった

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root to: 'home#index'
-  resources :messages, only: ['index']
-
+  resources :messages, only: ['index'] do
+    member do
+      resources :likes, only: ['create']
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
